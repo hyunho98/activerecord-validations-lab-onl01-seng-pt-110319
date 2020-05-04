82a19f18046b65217602ddb2849d5cb9372d72ee
Alex Lee <hyunho98@gmail.com>
2020-05-04 10:02:47 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,10 @@ class Post < ActiveRecord::Base
   validate :non_clickbait
 
   def non_clickbait
-    unless self.title.include?("Won't Believe") || self.title.include?("Secret") || self.title.include?("Top #{/\d/}") || self.title.include?("Guess")
-      errors.add(:title, "is not sufficiently clickbait-y")
+    if self.title != nil
+      unless self.title.include?("Won't Believe") || self.title.include?("Secret") || self.title.include?("Top #{/\d/}") || self.title.include?("Guess")
+        errors.add(:title, "is not sufficiently clickbait-y")
+      end
     end
 
   end
